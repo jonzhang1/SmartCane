c20de4948f2562a10abeab59fe297fdc890a8252
Krushil Amrutiya <krushilamrutiya@gmail.com>
2022-05-15 21:45:23 -0400

    Update smartCane.ino

--- smartCane.ino
+++ smartCane.ino
@@ -14,15 +14,23 @@ LiquidCrystal LCD(11,10,9,2,3,4,5);  //Create Liquid Crystal Object called LCD
 #define trigPin 13 //Sensor Echo pin connected to Arduino pin 13
 #define echoPin 12 //Sensor Trip pin connected to Arduino pin 12
 
-const int RELAY_PIN = 6;  //Pin number 6 for the relay which connects the third channel relay
-const int RELAY_PIN1 = 7; //Pin number 7 for the relay which connects the first channel relay
+const int RELAY_PIN = 7;  //Pin number 7 for the relay which connects the first channel relay
+const int RELAY_PIN1 = 4; //Pin number 4 for the relay which connects the second channel relay
+const int RELAY_PIN2 = 6; // Pin number 6 for the relay which connects the third channel relay
+const int RELAY_PIN3 = 5; // Pin number 5 for the relay which connects the fouth channel relay
 
 void setup() {  
+  
+  Serial.begin(9600);
+  
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
   
   pinMode(RELAY_PIN, OUTPUT);
   pinMode(RELAY_PIN1, OUTPUT);
+  pinMode(RELAY_PIN2, OUTPUT);
+  pinMode(RELAY_PIN3, OUTPUT);
+  
   
   LCD.begin(16,2); //Tell Arduino to start your 16 column 2 row LCD
   LCD.setCursor(0,0);  //Set LCD cursor to upper left corner, column 0, row 0
@@ -30,6 +38,7 @@ void setup() {
 }
 
 void loop() {
+  
   long duration, distance, distance_in;
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
@@ -41,24 +50,63 @@ void loop() {
   distance_in = distance * 0.393701;    
 
   LCD.setCursor(0,1);  //Set cursor to first column of second row
-  LCD.print("                "); //Print blanks to clear the row
+//  LCD.print("                "); //Print blanks to clear the row
   LCD.setCursor(0,1);   //Set Cursor again to first column of second row
 
+//  if (distance_in <= 5) {
+//    Serial.print(distance_in);
+//    Serial.print(" inches, ");
+    
+//    LCD.print(distance_in);
+//    LCD.print(" inches");  //Print your units.
+//    digitalWrite(RELAY_PIN, LOW);
+//    digitalWrite(RELAY_PIN, HIGH);
+//    digitalWrite(RELAY_PIN1, HIGH);
+//    digitalWrite(RELAY_PIN2, HIGH);
+//    digitalWrite(RELAY_PIN3, HIGH);
+    
+//  } 
+
   if (distance_in <= 5) {
-    LCD.print(distance_in);
-    LCD.print(" inches");  //Print your units.
+    Serial.print(" 5 or below ");
     digitalWrite(RELAY_PIN, LOW);
     digitalWrite(RELAY_PIN1, HIGH);
-    
-  } else if (distance_in <= 12) {
-    LCD.print(distance_in);
-    LCD.print(" inches");
-    digitalWrite(RELAY_PIN, HIGH);
+    digitalWrite(RELAY_PIN2, HIGH);
+    digitalWrite(RELAY_PIN3, HIGH);
+  }
+
+  else if (distance_in <= 9) {
+    Serial.print(" <= 9 ");
+    digitalWrite(RELAY_PIN, LOW);
     digitalWrite(RELAY_PIN1, LOW);
-  } else {
-    LCD.print("COME CLOSER");
+    digitalWrite(RELAY_PIN2, HIGH);
+    digitalWrite(RELAY_PIN3, HIGH);
+  }
+
+  else if (distance_in <= 11) {
+    Serial.print(" <= 11 ");
+    digitalWrite(RELAY_PIN, LOW);
+    digitalWrite(RELAY_PIN1, LOW);
+    digitalWrite(RELAY_PIN2, LOW);
+    digitalWrite(RELAY_PIN3, HIGH);
+  }
+
+  else if (distance_in <= 30) {
+    Serial.print(" <= 30 ");
+    digitalWrite(RELAY_PIN, LOW);
+    digitalWrite(RELAY_PIN1, LOW);
+    digitalWrite(RELAY_PIN2, LOW);
+    digitalWrite(RELAY_PIN3, LOW);
+  }
+ 
+  else {
+    Serial.print(" GET CLOSER ");
     digitalWrite(RELAY_PIN, HIGH);
     digitalWrite(RELAY_PIN1, HIGH);
+    digitalWrite(RELAY_PIN2, HIGH);
+    digitalWrite(RELAY_PIN3, HIGH);
   }
+
+  
   delay(250); //pause to let things settle
 }
